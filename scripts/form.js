792403b85a59bc1bef7c641851c4f98040f1fd86
Gabolit52 <gabriel.zevallos.work@gmail.com>
2025-06-15 00:19:40 +0100

    v

--- scripts/form.js
+++ scripts/form.js
@@ -1,9 +1,30 @@
 // Product array - id used for value, name used for option display
 const products = [
-  { id: "p1", name: "SuperWidget 3000" },
-  { id: "p2", name: "Gadget Pro X" },
-  { id: "p3", name: "UltraTool Deluxe" },
-  { id: "p4", name: "MegaDevice Plus" }
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
 
 // Populate the product select element dynamically
